37edfc7620523c6f24ac11df51b4fb8df4a423b2
lvyixinniang <117647132+lvyixinniang@users.noreply.github.com>
2024-04-03 19:42:46 +0800

    Add files via upload

---
+++ lanqiao13B_1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+// 1:无需package
+// 2: 类名必须Main, 不可修改
+
+public class lanqiao13B_1 {
+    public static void main(String[] args) {
+        System.out.println("7");
+    }
+}
