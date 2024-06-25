919efa0b939fd2ccd23908b50b1fd3623b717fc7
xyy <3426435900@qq.com>
2024-06-25 22:57:06 +0800

    add:leetcode21

---
+++ src/leetcode/leetcode21_link.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+
+
+public class leetcode21_link {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(4)));
+        ListNode list2 = new ListNode(1,
+                new ListNode(3,
+                        new ListNode(4)));
+        System.out.println(mergeTwoLists(list1, list2));
+    }
+    public static class ListNode {
+        int val;
+        leetcode21_link.ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, leetcode21_link.ListNode next) { this.val = val; this.next = next; }
+    }
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null||list2 ==null){
+            return list2!=null?list2:list1;
+        }
+
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (list1 != null && list2 != null){
+            if (list1.val <= list2.val) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+            
+        }
+        prev.next = list1 ==  null ? list2:list1;
+        return prehead.next;
+    }
+}
