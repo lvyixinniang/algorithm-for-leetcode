2f2993077bdea1a68f87b95004f0457cb562bc41
xyy <3426435900@qq.com>
2024-08-28 11:16:01 +0800

    add: 图论算法之岛屿数量 -- 传染法

--- src/leetcode/Graph/leetcode200.java
+++ src/leetcode/Graph/leetcode200.java
@@ -1,4 +1,44 @@
 package leetcode.Graph;
 
 public class leetcode200 {
+    public static void main(String[] args){
+        char[][] grid = {{'1','1','1','1','0'},
+        {'1','1','0','1','0'},
+        {'1','1','0','0','0'},
+        {'0','0','0','0','0'}};
+        System.out.println(numIslands(grid));
+    }
+
+    public static int numIslands(char[][] grid) {
+        int res =0;
+        int m  =grid.length;
+        int n = grid[0].length;
+        for (int y = 0; y< m;y++){
+            for (int x = 0; x< n;x++){
+                if (grid[y][x] == '1'){
+                    res++;
+                    infect(grid, x , y);
+                }
+            }
+        }
+        return res;
+    }
+    public static void infect(char[][] grid, int x , int y){
+        int m = grid.length , n = grid[0].length;
+        if (x < 0 || x>=n){
+            return ;
+        }
+        if (y < 0 || y >= m ){
+            return ;
+        }
+        if (grid[y][x] == '1'){
+            grid[y][x] ='.';
+        } else {
+            return ;
+        }
+        infect(grid,x + 1,y);
+        infect(grid,x - 1,y);
+        infect(grid,x,y + 1);
+        infect(grid,x,y - 1);
+    }
 }
