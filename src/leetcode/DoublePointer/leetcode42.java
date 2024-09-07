4fc2a965b39ac57ecb19371bf814917ebc0544d7
xyy <3426435900@qq.com>
2024-09-07 23:59:32 +0800

    add: 双指针 之 接雨水 , 采用 降序的单调栈, 来进行数组内部的 有序性 (双指针的 使用场景 有两个重要条件 内部有序性{这个并不是 单指降序,增序, 其他的比如符合一些数学公式也可以, 或者 特殊的排序 规则} , 以及 两个指针之间 要有关系 )

--- src/leetcode/DoublePointer/leetcode42.java
+++ src/leetcode/DoublePointer/leetcode42.java
@@ -1,28 +1,36 @@
 package leetcode.DoublePointer;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class leetcode42 {
     public static void main(String[] args) {
         System.out.println(trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
     public static int trap(int[] height) {
-        int sum = 0;
         int n = height.length;
-        int l = 0 , r = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (height[i] != 0) {
-                 l = i;
-                 r = l + 1;
-                 while (r < n){
-                     if (height[r] != 0){
-                         break;
-                     }
-                     r ++;
-                 }
-                 sum = sum + (r - l - 1)*Math.abs(height[l] - height[r]);
-                 l = r;
-            }
+        int[] flew = new int[n + 1];
+        Arrays.fill(flew , 0);
+
+        for (int i = 0; i < n; i++) {
+            flew[i] = height[i];
         }
 
-        return sum;
+        Deque<Integer> stack = new LinkedList<>();
+        int sum = 0;
+        for (int i = 0; i < n + 1; i++) {
+            while (!stack.isEmpty() && flew[stack.peek()] < flew[i]) {
+//               25 , 26 行, 代码的作用是 去除 栈中只有一个 元素的情况 , 因为, 这种 不可能有面积能生成
+                int  currIndex = stack.pop();
+                if (!stack.isEmpty()){
+                    int with = i - stack.peek() -1;
+                    int h = Math.min(flew[i], flew[stack.peek()]) - flew[currIndex];
+                    sum += with * h;
+                }
+            }
+            stack.push(i);
+        }
+        return  sum;
     }
 }
