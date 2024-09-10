5b0be81d99a0b78a5517b50b9a40e1dbfc845058
xyy <3426435900@qq.com>
2024-09-10 08:48:05 +0800

    add: 滑动窗口 之 找到字符串中所有字母异位词 , 使用了滑动窗口, 来优化时间效率

--- src/leetcode/SlidingWindow/leetcode438.java
+++ src/leetcode/SlidingWindow/leetcode438.java
@@ -4,29 +4,66 @@ import java.util.*;
 
 public class leetcode438 {
     public static void main(String[] args) {
-        System.out.println(findAnagrams("aa", "bb"));
+        System.out.println(findAnagrams("cbaebabacd", "abc"));
     }
     public static List<Integer> findAnagrams(String s, String p){
+/*
+//        TLE了
         List<Integer> indexs = new ArrayList<>();
         int window = p.length();
         char[] charArray = s.toCharArray();
         int n = charArray.length;
 
-        int[] eglish = new int[26];
-        Arrays.fill(eglish,0);
-
-        for (int i = 0; i < window; i++) {
-            int x = p.charAt(i) - 'a';
-            eglish[x]++;
+        Map<Character,Integer> judge = new HashMap<>();
+//        将 p 中的值 存入 judge中 a,b,c - > {0,1,2}
+        for (int i = 0; i < window; i++) {;
+            judge.put(charArray[i],i);
         }
 
         for (int i = 0; i < n; i++) {
-            int x = charArray[i] - 'a';
-            if (eglish[x] > 0) {
-                eglish[x]--;
+            if (judge.containsKey(charArray[i])) {
+                Map<Character,Integer> jd = new HashMap<>();
+                for (int j = 0; j < window; j++) {;
+                    jd.put(charArray[j],j);
+                }
+                for (int j = i;j < n && j < i + window; j++) {
+                    if (jd.containsKey(charArray[j])) {
+//                        这个是 sb ArrayList 有个重载 remove 经常识别错 , 加"aaaa",是防止这个现象
+                        jd.remove(charArray[j]);
+                    }
+                }
+                if (jd.size() == 0) {
+                    indexs.add(i);
+                }
             }
         }
+        return indexs;*/
 
-        return indexs;
+        List<Integer> res = new ArrayList<>();
+        int np = p.length(), ns = s.length();
+        if (ns < np) {
+            return res;
+        }
+        final int[] target = count(p, 0, np);
+        final int[] sCount = count(s, 0, np - 1);
+        for (int i = 0; i < ns - np + 1; i++) {
+//            移动 右指针 添加右参数
+            ++sCount[s.charAt(i + np - 1) - 'a'];
+            if (Arrays.equals(target, sCount)) {
+                res.add(i);
+            }
+//            移动 左指针 删除左参数
+            --sCount[s.charAt(i) - 'a'];
+        }
+        return res;
+    }
+//    方便 每次记录26 位 的数量
+    public static int[] count(String s, int start, int end) {
+        int[] res = new int[26];
+        for (int i = 0; i < end; i++) {
+            final int c = s.charAt(i) -'a';
+            res[c] ++;
+        }
+        return res;
     }
 }
