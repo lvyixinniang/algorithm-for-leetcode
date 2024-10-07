3251e35a1ac8a5e5a4cff0f1f7fbbfc3fb2084c5
xyy <3426435900@qq.com>
2024-10-07 10:53:46 +0800

    add: 二叉树 之 对称二叉树

---
+++ src/leetcode/Binary_Tree/leetcode226.java
@@ -0,0 +1,29 @@
+package leetcode.Binary_Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class leetcode226 {
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = right;
+        root.right = left;
+        invertTree(left);
+        invertTree(right);
+        return root;
+    }
+}
