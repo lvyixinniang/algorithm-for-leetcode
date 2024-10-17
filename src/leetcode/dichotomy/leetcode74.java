902996c00ac1cf2dc9a78ed02a03d88386ecd9c9
xyy <3426435900@qq.com>
2024-10-17 16:37:46 +0800

    add: 二分法 之 在排序数组中查找元素的第一个和最后一个位置 （双指针）

--- src/leetcode/dichotomy/leetcode74.java
+++ src/leetcode/dichotomy/leetcode74.java
@@ -1,32 +1,29 @@
 package leetcode.dichotomy;
 
 public class leetcode74 {
-    public static void main(String[] args) {
-        int[][] matrix = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        System.out.println(searchMatrix(matrix, 3));
-    }
 
     public static boolean  searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length,n=matrix[0].length;
-        int[] nums = new int[m*n];
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                nums[i*n+j] = matrix[i][j];
+        int r = matrix.length, l = matrix[0].length;
+        int[] arr = new int[r * l];
+        for (int i = 0; i < r; i++) {
+            for (int k = 0; k < l; k++) {
+                arr[i*l + k] = matrix[i][k];
             }
         }
-
-        int l = 0 , r = m*n -1;
-        int mid;
-        while (l<r) {
-            mid = (r - l)>>2 +l;
-            if (target == nums[mid]) return true;
-            else if (target < nums[mid]){
-                r = mid -1;
+        int le = 0, ri =  r*l - 1;
+        while (le <= ri) {
+            int mid = (ri - le) / 2 + le;
+            if (arr[mid] == target) return true;
+            else if (arr[mid] > target) {
+                ri = mid - 1;
             }else {
-                l = mid + 1;
+                le = mid + 1;
             }
         }
-
         return false;
+    }
+
+    public static void main(String[] args) {
+        searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}}, 30);
     }
 }
