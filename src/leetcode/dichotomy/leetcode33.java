dd3d0d36528aacd5c3775853ea8b814c2ac3cbe2
xyy <3426435900@qq.com>
2024-10-17 16:49:22 +0800

    add: 二分法 之 搜索旋转排序数组

---
+++ src/leetcode/dichotomy/leetcode33.java
@@ -0,0 +1,33 @@
+package leetcode.dichotomy;
+
+import java.util.Arrays;
+
+public class leetcode33 {
+
+    public int search(int[] nums, int target) {
+        int exist = -1;
+//        肯定是要先找到 K 值
+        int length = nums.length;
+        int k = 0;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                k = i;
+            }
+        }
+        Arrays.sort(nums);
+        int l = 0, r = length - 1;
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] == target) {
+                exist = (mid + k) % length;
+                break;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            }else {
+                l = mid + 1;
+            }
+        }
+
+        return exist;
+    }
+}
