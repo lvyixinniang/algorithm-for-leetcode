39eee0cb15c72bfbdca1caee6b1a6eb3294d8374
xyy <3426435900@qq.com>
2024-11-15 15:52:11 +0800

    add: LCR剑指offer 之 寻找文件副本

---
+++ src/leetcode/LCR/leetcode120.java
@@ -0,0 +1,18 @@
+package leetcode.LCR;
+
+import com.sun.org.apache.xerces.internal.impl.xs.AttributePSVImpl;
+
+public class leetcode120 {
+    public static void main(String[] args) {
+        System.out.println(findRepeatDocument(new int[]{1, 1, 1}));
+    }
+    public static int findRepeatDocument(int[] documents) {
+        // 查找重复数字 桶排
+        int[] arr = new int[100000];
+        for (int i : documents) {
+            if (arr[i] == 0) arr[i] = 1;
+            else return i;
+        }
+        return -1;
+    }
+}
