6618e09215ccf7c1d0d8c167c11b41939325ca77
xyy <3426435900@qq.com>
2024-11-16 23:55:51 +0800

    add: LCR剑指offer 之 图书整理|

---
+++ src/leetcode/LCR/leetcode130.java
@@ -0,0 +1,21 @@
+package leetcode.LCR;
+
+public class leetcode130 {
+//    todo 不会做
+    public static void main(String[] args) {
+        System.out.println(wardrobeFinishing(16, 8, 4));
+    }
+    public static int wardrobeFinishing(int m, int n, int cnt) {
+        int count = 1;
+        if (cnt < 10);
+        return 1;
+    }
+    public static int digit(int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum += (x % 10);
+            x /= 10;
+        }
+        return sum;
+    }
+}
