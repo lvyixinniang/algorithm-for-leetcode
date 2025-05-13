69f1dba909bc91d1e12ccf80844a351b0a680259
huangchuanw <huangchuanw@yonyou.com>
2025-05-13 09:56:33 +0800

    add:堆排查找，快排查找

---
+++ src/interface150/tent15.java
@@ -0,0 +1,11 @@
+package interface150;
+
+public class tent15 {
+/*    给定一个可能含有重复值的数组 arr，找到每一个 i 位置左边和右边离 i 位置最近且值比
+    arr[i] 小的位置。返回所有位置相应的信息。位置信息包括：两个数字 L 和 R，
+    如果不存在，则值为 -1，下标从 0 开始。*/
+    public int[][] foundMonotoneStack (int[] nums) {
+        // write code here
+        return null;
+    }
+}
